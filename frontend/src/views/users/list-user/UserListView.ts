9f140cdb82d5752935adb537bf701f1695cb8583
JefersonBLuz <jefersonbrluz@gmail.com>
2024-12-13 17:05:49 -0300

    Função ativar/inativar usuário

--- frontend/src/views/users/list-user/UserListView.ts
+++ frontend/src/views/users/list-user/UserListView.ts
@@ -3,7 +3,7 @@ export default {
     name: 'UserListView',
     data() {
         return {
-            users: []
+            users: [],
         };
     },
     mounted() {
@@ -14,19 +14,38 @@ export default {
             try {
                 const response = await UserService.getUsresAddressAll()
                 console.log(response);
-                
+
                 this.users = response;
             } catch (error) {
                 console.error('Error ao listar usuários: ', error);
             }
         },
-        async removerUser(id: number){
+        async removerUser(id: number) {
             try {
                 await UserService.deleteUser(id)
                 console.log('delete');
                 this.listAllUsers()
             } catch (error) {
                 console.error('Error ao remover usuário: ', error);
+            }
+        },
+        async blockUser(user: any) {
+            try {
+                const response = await UserService.getUsresAddressId(user);
+                const userForm = {...response[0].users}
+                console.log(userForm);
+                console.log(userForm.active);
+                if(userForm.active){
+                    userForm.active = false
+                }else{
+                    userForm.active = true
+                }
+                console.log(userForm.active);
+                console.log(userForm);
+                await UserService.updateUser(userForm)
+                this.listAllUsers()
+            } catch (error) {
+
             }
         }
     }
