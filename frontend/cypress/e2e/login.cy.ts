57f0b6d0f16cb9ee80826a8f45dddbee05d5a0a7
Rafael <rafael.s.santos6@ba.estudante.senai.br>
2024-12-17 15:39:02 -0300

    Criação de testes com cypress

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,7 +1,23 @@
-describe('template spec', () => {
+describe('Projeto TCC - Testando login', () => {
   it('Testando login com senha e email incorretos', () => {
     cy.visit('http://localhost:5173/')
     cy.get('#email').type("João")
     cy.get('#password').type("12345")
+    cy.get(':nth-child(3) > .flex').click()
+    cy.get(':nth-child(3) > .text-center').should("be.visible").and('contain', "Usuário/Senha incorretos!")
+
   })
+
+  it("Testando login com senha e email válidos", () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('#email').type("admin")
+    cy.get('#password').type("admin")
+    cy.get(':nth-child(3) > .flex').click()
+
+  })
+    
+
+
 })
+
+
