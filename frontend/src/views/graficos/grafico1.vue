a3bb6ca6ef0f4cce67f8f98a403d3ad7ce617bd9
JefersonBLuz <jefersonbrluz@gmail.com>
2025-02-12 16:58:27 -0300

    Update graf

--- frontend/src/views/graficos/grafico1.vue
+++ frontend/src/views/graficos/grafico1.vue
@@ -1,4 +1,4 @@
 <template>
-    <iframe title="tccMapa" width="1280" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiYzZlMjMxOGItOTFjMy00ODJhLWJlMjMtNWQ2NzQwOGRhY2RkIiwidCI6IjhhYWVlMzMzLWYxODktNDI1OS1hZWEyLTFiOTAwYjI3NzZiZSJ9" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="tccMapa" width="1280" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiOWY3YTNhNDQtN2M1Yi00Yzc2LTg1ZTAtZDUzNjU2NzJiOTJiIiwidCI6IjE0YzI4YjJiLWViMDQtNDdlMy1iMjkxLWU0Zjk0YmYwMDQ1NiJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     
 </template>
--- frontend/src/views/graficos/grafico2.vue
+++ frontend/src/views/graficos/grafico2.vue
@@ -1,4 +1,4 @@
 <template>
     <!-- <iframe title="tcc" width="1280" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiYmIxOTkzYjktZWI5NC00NjEzLTgyZTItZGM2NDA4MGQ0ZTEyIiwidCI6IjhhYWVlMzMzLWYxODktNDI1OS1hZWEyLTFiOTAwYjI3NzZiZSJ9" frameborder="0" allowFullScreen="true"></iframe> -->
-    <iframe title="testetsetse" width="1280" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiOThjNWM2MDQtMjA1My00N2MwLTg1NGEtOTU1NWMxMWVlYWIxIiwidCI6IjhhYWVlMzMzLWYxODktNDI1OS1hZWEyLTFiOTAwYjI3NzZiZSJ9" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="testetsetse" width="1280" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiOWY3YTNhNDQtN2M1Yi00Yzc2LTg1ZTAtZDUzNjU2NzJiOTJiIiwidCI6IjE0YzI4YjJiLWViMDQtNDdlMy1iMjkxLWU0Zjk0YmYwMDQ1NiJ9" frameborder="0" allowFullScreen="true"></iframe>
 </template>
