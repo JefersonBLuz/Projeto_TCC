781ae27ca313532b77ee38cf7dc879a06d692a54
JefersonBLuz <jefersonbrluz@gmail.com>
2025-02-12 16:54:41 -0300

    aplication update

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import router from './router'
+import { populationBomb } from './config/seed'
 
 export class App {
     private server: express.Application
@@ -8,6 +9,7 @@ export class App {
     constructor() {
         this.server = express()
         this.middleware()
+        // this.seed()
         this.router()
     }
     private middleware(){
@@ -20,5 +22,8 @@ export class App {
     }
     public getServer(): express.Application {
         return this.server
+    }
+    private async seed(){
+      await populationBomb()
     }
 }
